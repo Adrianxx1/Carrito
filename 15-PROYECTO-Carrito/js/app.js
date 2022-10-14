9907c3138fb48b35c78ee1043b0210a7b0465026
Luis Adrian Amador <triaxis12q@gmail.com>
2022-10-14 12:42:39 -0500

    funcionalidad cantidad de carrito

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -1,4 +1,3 @@
-// variables para referenciar a objetos del documeto
 const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
@@ -8,51 +7,72 @@ let listadoCarrito = [];
 
 const agregarCurso = (e) => {
     e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
-        const curso = e.target.parentElement; 
+    if(e.target.classList.contains('agregar-carrito')) {
+        const curso = e.target.parentElement.parentElement;
         const infoCurso = {
-            imagen: curso.querySelector('img').src, 
-            nombre: curso.querySelector('h4').textContent,
-            precio: curso.querySelector('p.precio').textContent,
-            id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),
-            cantidad: 1
+        imagen: curso.querySelector('img').src,
+        nombre: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('p.precio').textContent,
+        id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),
+        cantidad : 1
         }
-        
-        agregarCarrito(infoCurso);    
-}
+        agregarCarrito(infoCurso);
+    }
+
+
 }
 
-const agregarCarrito = curso =>{ 
-    listadoCarrito = [...listadoCarrito, curso]
-    console.log(listadoCarrito);
-    generaHTML();
-}
-
-const generaHTML = () => {
-    vaciarCarrito();
-    listadoCarrito.forEach(curso => {
-        const row = document.createElement('tr');
-        const cursoHTML = `
-        <td>
-            <img src="${curso.imagen}= width=100>
+  // console.log("Curso a agregar");
+        //console.log(curso.id)
+        //console.log("Listado de cursos");
+        //listadoCarrito.forEach (curso => console.log(curso.id) );
+        if(listadoCarrito.some(cursoInCarrito => cursoInCarrito.id === curso.id)) {
+            let carrito = listadoCarrito.map(cursoInCarrito => {
+                if(cursoInCarrito.id == curso.id) {
+                    cursoInCarrito.cantidad++;
+                    return cursoInCarrito;
+                }else{
+                    return cursoInCarrito;
+                }
+            })
+                listadoCarrito = [...carrito];
+            } else {
+                listadoCarrito = [...listadoCarrito, curso];
+            }
+            console.log(listadoCarrito);
+            generHTML();
+        }
+        const generHTML = () => {
+            vaciarCarrito();
+            listadoCarrito.forEach (curso => {
+                const row = document.createElement('tr');
+                const cursoHTML = ` 
+                <td>
+                 <img src="${curso.imagen}" width= 100>
+                </td>
+                <td>
+                ${curso.nombre}
+            </td>
+            <td>
+            ${curso.precio}
         </td>
-        <td>${curso.nombre}</td>
-        <td>${curso.precio}</td>
-        <td>${curso.cantidad}</td>
-        `;
-        row.innerHTML = cursoHTML;
-        contenedorCarrito.appendChild(row);
-    }); 
-}
+        <td>
+        ${curso.cantidad}
+    </td>
+                `;
+                row.innerHTML = cursoHTML;
+                contenedorCarrito.appendChild(row);
+        });
+    }
+    
+        const vaciarCarrito = () => {
 
-const vaciarCarrito = () => {
-    contenedorCarrito.innerHTML = '';
+            contenedorCarrito.innerHTML = ""; }
 
-}
 
-const cargarEventListener = () => {
-// Agregar funcion de carga de cursos al carrito
-listaCursos.addEventListener('click', agregarCurso);
-}
 
-cargarEventListener();
+            const cargarEventListener = () => {
+                //Agregar funcion de carga de cursos al carrito
+                listaCursos.addEventListener('click', agregarCurso);
+            }
+            cargarEventListener();
