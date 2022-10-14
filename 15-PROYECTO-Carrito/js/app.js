6bdc3e38c4328e2c86000a358984c2b7ecfb4ce6
Luis Adrian Amador <triaxis12q@gmail.com>
2022-10-14 12:44:42 -0500

    implementando storage

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -17,62 +17,85 @@ const agregarCurso = (e) => {
         cantidad : 1
         }
         agregarCarrito(infoCurso);
+
     }
 
-
 }
-
-  // console.log("Curso a agregar");
+    const agregarCarrito = curso => {
+       // console.log("Curso a agregar");
         //console.log(curso.id)
         //console.log("Listado de cursos");
         //listadoCarrito.forEach (curso => console.log(curso.id) );
         if(listadoCarrito.some(cursoInCarrito => cursoInCarrito.id === curso.id)) {
-            let carrito = listadoCarrito.map(cursoInCarrito => {
-                if(cursoInCarrito.id == curso.id) {
-                    cursoInCarrito.cantidad++;
-                    return cursoInCarrito;
-                }else{
-                    return cursoInCarrito;
-                }
-            })
-                listadoCarrito = [...carrito];
-            } else {
-                listadoCarrito = [...listadoCarrito, curso];
+        let carrito = listadoCarrito.map(cursoInCarrito => {
+            if(cursoInCarrito.id == curso.id) {
+                cursoInCarrito.cantidad++;
+                return cursoInCarrito;
+            }else{
+                return cursoInCarrito;
             }
-            console.log(listadoCarrito);
-            generHTML();
+        })
+            listadoCarrito = [...carrito];
+        } else {
+            listadoCarrito = [...listadoCarrito, curso];
         }
-        const generHTML = () => {
-            vaciarCarrito();
-            listadoCarrito.forEach (curso => {
-                const row = document.createElement('tr');
-                const cursoHTML = ` 
-                <td>
-                 <img src="${curso.imagen}" width= 100>
-                </td>
-                <td>
-                ${curso.nombre}
-            </td>
-            <td>
-            ${curso.precio}
-        </td>
-        <td>
-        ${curso.cantidad}
-    </td>
-                `;
-                row.innerHTML = cursoHTML;
-                contenedorCarrito.appendChild(row);
-        });
+        console.log(listadoCarrito);
+        generHTML();
     }
+    const generHTML = () => {
+        vaciarCarrito();
+        localStorage.setItem('carrito', JSON.stringify(listadoCarrito));
+        listadoCarrito.forEach (curso => {
+            const row = document.createElement('tr');
+            const cursoHTML = ` 
+            <td>
+            <img src="${curso.imagen}" width= 100>
+            </td>
+
+            <td>${curso.nombre} </td>
+        <td>${curso.precio}</td>
+    <td>${curso.cantidad}</td>
+    <td>
+           <a href = "#" class ="borrar-curso" data id="$(curso.id)">x</a
+           </td>
+
+            `;
+            row.innerHTML = cursoHTML;
+            contenedorCarrito.appendChild(row);
+
+    });
+}
+
+    const vaciarCarrito = () => {
+        while(contenedorCarrito.firstChild) {
+            contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+        }
+    }
+
+    const eliminarCurso = (e) => { 
+        e.preventDefault();
+        if(e.target.classList.contains('borrar-curso')) {
+            let idCurso = e.target.getAttribute('data-id')
+            let carrito = listadoCarrito.filter(cursoInCarrito => cursoInCarrito.id !== idCurso)
+        listadoCarrito = [...carrito];
+        generHTML();
+    }
+}
     
-        const vaciarCarrito = () => {
-
-            contenedorCarrito.innerHTML = ""; }
+    
 
 
+const cargarEventListener = () => {
+    //Agregar funcion de carga de cursos al carrito
 
-            const cargarEventListener = () => {
-                //Agregar funcion de carga de cursos al carrito
-                listaCursos.addEventListener('click', agregarCurso);
-            }
-            cargarEventListener();
+    listaCursos.addEventListener('click', agregarCurso);
+    contenedorCarrito.addEventListener('click', eliminarCurso);
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+    const carritoInStorage = localStorage.getItem('carrito')
+    if(carritoInStorage) {
+        listadoCarrito = JSON.parse(carritoInStorage);
+        generHTML();
+    }
+}
+cargarEventListener();
